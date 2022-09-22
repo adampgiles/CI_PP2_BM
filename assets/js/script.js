44d7f9e25aa369298f55ed67dccdd90480432f56
adampgiles <adampgiles@outlook.com>
2022-09-22 15:44:50 +0100

    feat: added refresh button to tempo-block. script: added code to update tempo when refresh clicked, also locks value with min/max.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Playback variables
-const playbackInput = document.getElementById("input-playback");
+const inputPlayback = document.getElementById("input-playback");
 let iconPlay = document.getElementById("icon-play");
 let iconStop = document.getElementById("icon-stop");
 let isPlaying = false;
@@ -7,6 +7,21 @@ let isPlaying = false;
 // Tempo variable
 let tempoInput = document.getElementById("input-tempo");
 let beatsPerMinute = (60 / tempoInput.value) * 1000; // Converts tempo to milliseconds
+let inputTempoRefresh = document.getElementById("input-tempo-refresh");
+let tempoMinMax = {"min": 60 , "max": 200}; 
+inputTempoRefresh.addEventListener("click", function(){
+  // Lock tempo to min and max values
+  if(tempoInput.value < tempoMinMax.min){
+    tempoInput.value = tempoMinMax.min;
+  }
+  else if(tempoInput.value > tempoMinMax.max){
+    tempoInput.value = tempoMinMax.max;
+  } 
+  beatsPerMinute = (60 / tempoInput.value) * 1000;
+
+  console.log("Current Tempo: " + tempoInput.value); // Test if new value updated
+});
+
 
 // Kit variables
 let kitSelect = document.getElementById("kits");
@@ -18,7 +33,7 @@ window.addEventListener('DOMContentLoaded', function(){
   iconStop.style.display = "none";
 });
 
-playbackInput.addEventListener("click", TogglePlayback);
+inputPlayback.addEventListener("click", TogglePlayback);
 function TogglePlayback(){
   if(isPlaying){  
     iconPlay.style.display = "block";
@@ -30,7 +45,7 @@ function TogglePlayback(){
     iconStop.style.display = "block";
     isPlaying = true;
   }
-  console.log("Playing: " + isPlaying);
+  console.log("Playing: " + isPlaying); // Test if playback changed
 }
 
 // Updates currentKit when new option selected on combobox
@@ -47,7 +62,7 @@ kitSelect.addEventListener('input', function(){
   if(kitSelect.value == "dubstep"){
     // Set Kit to DUBSTEP
   }   
-  console.log("Current Kit:" + kitSelect.value);
+  console.log("Current Kit: " + kitSelect.value); // Test if kit updated
 });
 
 
