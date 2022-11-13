1c97538b11e6656c426209ef38b1e62495a98501
adampgiles <adampgiles@outlook.com>
2022-11-13 09:49:52 +0000

    code: added code to show and hide submission window. Also added submit button to submission form. Set submission-input-block to only show with Preset/Share tab

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,16 +2,21 @@
 drumWindow = document.getElementById("drum-window");
 shareWindow = document.getElementById("share-window");
 infoWindow = document.getElementById("info-window");
+submissionInputBlock = document.getElementById("submission-input-block");
+submissionWindow = document.getElementById("submission-window");
 
 inputDrumTab = document.getElementById("drum-tab");
 inputShareTab = document.getElementById("share-tab");
 inputInfoTab = document.getElementById("info-tab");
+inputSubmissionOpen = document.getElementById("input-submission-open");
+inputSubmissionClose = document.getElementById("input-submission-close");
 
 inputDrumTab.addEventListener("click", ShowDrumWindow);
 function ShowDrumWindow(){
   drumWindow.style.display = "flex";
   shareWindow.style.display = "none";
   infoWindow.style.display = "none";
+  submissionInputBlock.style.display = "none"
 
   inputDrumTab.style.zIndex = "2";
   inputShareTab.style.zIndex = "0";
@@ -27,6 +32,7 @@ function ShowShareWindow(){
   drumWindow.style.display = "none";
   shareWindow.style.display = "flex";
   infoWindow.style.display = "none";
+  submissionInputBlock.style.display = "flex"
 
   inputDrumTab.style.zIndex = "0";
   inputShareTab.style.zIndex = "2";
@@ -42,6 +48,7 @@ function ShowInfoWindow(){
   drumWindow.style.display = "none";
   shareWindow.style.display = "none";
   infoWindow.style.display = "flex";
+  submissionInputBlock.style.display = "none"
   
   inputDrumTab.style.zIndex = "0";
   inputShareTab.style.zIndex = "0";
@@ -50,4 +57,13 @@ function ShowInfoWindow(){
   inputDrumTab.style.backgroundColor = "rgb(110, 110, 110)";
   inputShareTab.style.backgroundColor = "rgb(110, 110, 110)";
   inputInfoTab.style.backgroundColor = "rgb(130, 130, 130)";
+}
+
+inputSubmissionOpen.addEventListener("click", ShowSubmissionWindow);
+inputSubmissionClose.addEventListener("click", HideSubmissionWindow);
+function ShowSubmissionWindow(){
+  submissionWindow.style.display = "flex";
+}
+function HideSubmissionWindow(){
+  submissionWindow.style.display = "none";
 }
