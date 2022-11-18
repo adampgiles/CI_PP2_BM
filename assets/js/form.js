1cac852f4a2a780f65e230b23758c9349ed0e61d
adampgiles <adampgiles@outlook.com>
2022-11-18 16:20:08 +0000

    fix: added validation to submission form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,40 @@
 function SendLoop(){
-  ExportLoop();
-  var parameters = {
-    name: document.getElementById("name").value,
-    emailAddress: document.getElementById("email-address").value,
-    loopTitle: document.getElementById("loop-title").value,
-    loopData: document.getElementById("input-share-data").value
-  };
-
-  serviceID = "service_n6ymtsk";
-  templateID = "template_ftm6gxd";
+  inputname = document.getElementById("name").value,
+  inputEmailAddress = document.getElementById("email-address").value,
+  inputLoopTitle = document.getElementById("loop-title").value,
+  inputLoopData = document.getElementById("input-share-data").value
   formAlert = document.getElementById("form-alert");
+
+  if(inputname == "" || inputEmailAddress == "" || inputLoopTitle == ""){
+    formAlert.textContent = "Please enter details in all three text boxes";
+    return;
+  }
+  else{
+    formAlert.textContent = "Please wait, sending details...";
+
+    ExportLoop();
+    var parameters = {
+      name: inputname.value,
+      emailAddress: inputEmailAddress.value,
+      loopTitle: inputLoopTitle.value,
+      loopData: inputLoopData.value
+    };
   
-  emailjs.send(serviceID, templateID, parameters)
-  .then(
-    res =>{
-      document.getElementById("name").value = "";
-      document.getElementById("email-address").value = "";
-      document.getElementById("loop-title").value = "";
-      console.log(res);
-  
-      formAlert.textContent = "Loop Data successfully sent for review";
-    }
-  )
-  .catch((err) => console.log(err));
+    serviceID = "service_n6ymtsk";
+    templateID = "template_ftm6gxd";    
+    
+    emailjs.send(serviceID, templateID, parameters)
+    .then(
+      res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email-address").value = "";
+        document.getElementById("loop-title").value = "";
+        console.log(res);
+    
+        formAlert.textContent = "Loop Data successfully sent for review! Thank you!";
+      }
+    )
+    .catch((err) => console.log(err));
+  }
 }
 
